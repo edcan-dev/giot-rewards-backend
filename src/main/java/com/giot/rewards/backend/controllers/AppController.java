3685a7d3a616a2b01e21f43b8043082dccf5f354
edcan-dev <israel.da.queenie@gmail.com>
2023-03-08 20:37:38 -0600

    asd

--- src/main/java/com/giot/rewards/backend/controllers/AppController.java
+++ src/main/java/com/giot/rewards/backend/controllers/AppController.java
@@ -14,10 +14,10 @@ import java.util.List;
 @RequestMapping("/")
 public class AppController {
 
-    @Autowired
-    private UserRepository userRepository;
-    @Autowired
-    private CredentialRepository credentialRepository;
+    //@Autowired
+    //private UserRepository userRepository;
+    //@Autowired
+    //private CredentialRepository credentialRepository;
 
     /*@GetMapping("/list")
     public String list() {
@@ -33,31 +33,30 @@ public class AppController {
         return result;
     }*/
 
-    @GetMapping("/list")
+    /*@GetMapping("/list")
     public List<User>list() {
         //return userRepository.findAll();
         //Users userEx = new Users(12312,"Iván","Larios","student","","",100);
         //userRepository.save(userEx);
         return userRepository.findAll();
+    }*/
 
-    }
-
-    @GetMapping("/post")
+    /*@GetMapping("/post")
     public void post() {
         List<User> list = new ArrayList<>();
         list.add(new User(201920495,"Iván","Larios","student","","",100));
         list.add(new User(201920499,"Rams","Ramos","student","","",100));
         userRepository.insert(list);
-    }
+    }*/
 
-    @GetMapping("/postCredential")
+    /*@GetMapping("/postCredential")
     public void post2() {
         credentialRepository.insert(new Credential(201920732,"edcan","pass123"));
         credentialRepository.insert(new Credential(201920495,"fluffy","pass123"));
         credentialRepository.insert(new Credential(201920499,"rams","pass123"));
-    }
+    }*/
 
-    @GetMapping("/login")
+    /*@GetMapping("/login")
     public String index(@RequestParam(value="username", required = false) String username,@RequestParam(value="password", required = false) String password) {
 
         if(username != null && password != null) {
@@ -65,5 +64,12 @@ public class AppController {
         } else {
             return "GIoT Rewards Backend - Esperando Credenciales...";
         }
+    }*/
+
+    @PostMapping ("/response/postbody")
+    public String post(@RequestBody String name, @RequestBody String lastname) {
+        System.out.println(name);
+        System.out.println(lastname);
+        return "Hello " + name + " " + lastname;
     }
 }
